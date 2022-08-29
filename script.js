9fc336ad981866b377e99e40fc201edfdabb2213
CodeZhifty <juulstausholmoscar@gmail.com>
2022-08-29 23:17:05 +0200

    Joke and note triggers added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var defaultSpeechEngine = new webkitSpeechRecognition();
 defaultSpeechEngine.lang = 'en-US';
 
 //Wake words
-const wakeWords = ["hello michael", "ok michael"];
+const wakeWords = ["hello michael", "ok michael", "michael"];
 
 //Button to start my AI
 const startBtn = document.getElementById("startBtn");
@@ -11,10 +11,15 @@ startBtn.addEventListener("click", () => {
     defaultSpeechEngine.start();
 });
 
+//Reply, if true then reply system is on
+let reply = false;
+
 //Restart speech engine onend
 defaultSpeechEngine.onend = function() {
-    console.log("Ended");
-    defaultSpeechEngine.start();
+    if(reply == false) {
+        console.log("Ended");
+        defaultSpeechEngine.start();
+    }
 }
 
 //Default recognition results (Run trigger or speak response)
@@ -22,11 +27,13 @@ defaultSpeechEngine.onresult = function(event) {
     var result = event.results[0][0].transcript.toLowerCase();
     console.log(result);
 
-    wakeWords.forEach(element => {
+    for (let i = 0; i < wakeWords.length; i++) {
+        const element = wakeWords[i];
         if(result.includes(element) && result.length > element.length) {
             runChatBot(result);
+            break;
         }
-    });
+    }
 }
 
 //Run neural net and then speak or call trigger
@@ -58,14 +65,75 @@ function open_minecraft(response, result) {
 }
 
 function weather(response, result) {
-    weatherFromSentence("Silkeborg", "f4e80e2071fcae0bd7c122d2f82fd284")
+    const city = "silkeborg";
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f4e80e2071fcae0bd7c122d2f82fd284`)
+    .then(response => response.json())
     .then(data => {
-        speak(`The temperature in ${data.city} is ${Math.floor(data.weather.main.temp)} degrees, with ${data.weather.info.description}`);
-    })
+        speak(`The temperature in ${city} is ${Math.floor(data.main.temp)} degrees, with ${data.weather[0].description}`);
+    });
 }
 
 function time(response, result) {
     const date = new Date();
-    const time = date.getHours() + " " + date.getMinutes();
+
+    let time;
+    if(toString(date.getMinutes()).length == 1) {
+        time = date.getHours() + " o " + date.getMinutes();
+    } else {
+        time = date.getHours() + " " + date.getMinutes();
+    }
+
     speak(`the time is ${time}`);
+}
+
+let notes = [];
+function note(response, result) {
+    var noteSpeechEngine = new webkitSpeechRecognition();
+    noteSpeechEngine.lang = 'en-US';
+    speak("What should the note contain?")
+    reply = true;
+    defaultSpeechEngine.stop();
+    noteSpeechEngine.start();
+
+    noteSpeechEngine.onresult = function(event) {
+        var result = event.results[0][0].transcript.toLowerCase();
+        console.log(result);
+        notes.push(result);
+        speak("note is created");
+        console.log(notes);
+        reply = false;
+        defaultSpeechEngine.start();
+    }
+}
+
+function readnote(response, result) {
+    if(notes.length > 0) {
+        if(notes.length == 1) {
+            speak(`you currently have ${notes.length} note`);
+            notes.forEach((note, i) => {
+                speak(`the note is ${note}`);
+            });
+        } else {
+            speak(`you currently have ${notes.length} notes`);
+            notes.forEach((note, i) => {
+                speak(`number ${i + 1} note is ${note}`);
+            });
+        }
+    } else {
+        speak("you have no notes sir");
+    }
+}
+
+function clearnote(response, result) {
+    notes = [];
+    speak("your notes has been cleared");
+}
+
+function joke() {
+    fetch("https://v2.jokeapi.dev/joke/Any?type=twopart")
+    .then(response => response.json())
+    .then(data => {
+        speak(data.setup);
+        speak(data.delivery);
+    })
 }
